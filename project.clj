b78bd9c26aa5661b34ac8413e3bb63a3c25e0495
Milt Reder <milt@yetanalytics.com>
2016-12-21 01:42:26 -0500

    incanter -> core.matrix

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject succession "0.1.0"
+(defproject com.yetanalytics/succession "0.1.1-SNAPSHOT"
   :description "Statistics for time series data"
   :url "http://github.com/henrygarner/succession"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [incanter "1.5.6"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [net.mikera/core.matrix "0.57.0"]])
--- src/succession/core.clj
+++ src/succession/core.clj
@@ -1,20 +1,20 @@
 (ns succession.core
-  (:require [incanter.core :as i]
-            [incanter.stats :as s]))
+  (:require [clojure.core.matrix :as m]
+            [clojure.core.matrix.stats :as ms]))
 
 (defn log-likelihood* [{:keys [vt ft at pt t z h]} y]
-  (let [v  (i/minus y (i/mmult z at))
-        zt (i/trans z)
-        f  (inc (first (i/mmult z pt zt)))
-        kt (-> (i/mmult t pt zt)
-               (i/plus h)
-               (i/div f))
-        at (i/plus (i/mmult t at)
-                   (i/mmult kt v))
-        lt (i/minus t (i/mmult kt z))
-        jt (i/minus h kt)
-        pt (i/plus (i/mmult t pt (i/trans lt))
-                   (i/mmult h (i/trans jt)))]    
+  (let [v  (m/sub y (m/mmul z at))
+        zt (m/transpose z)
+        f  (inc (first (m/mmul z pt zt)))
+        kt (-> (m/mmul t pt zt)
+               (m/add h)
+               (m/div f))
+        at (m/add (m/mmul t at)
+                  (m/mmul kt v))
+        lt (m/sub t (m/mmul kt z))
+        jt (m/sub h kt)
+        pt (m/add (m/mmul t pt (m/transpose lt))
+                  (m/mmul h (m/transpose jt)))]
     {:vt (cons v vt)
      :ft (cons f ft)
      :at at :pt pt :t t :h h :z z}))
@@ -23,24 +23,24 @@
   (let [m     (max p q)
         phi   (take m (concat (take p coefs) (repeat 0)))
         theta (take m (concat (drop p coefs) (repeat 0)))
-        init {:at (i/matrix 0 m 1)
-              :pt (i/identity-matrix m)
-              :t  (i/bind-columns
-                   (i/matrix phi)
-                   (i/bind-rows
-                    (i/identity-matrix (dec m))
-                    (i/matrix 0 1 (dec m))))
-              :h (i/plus phi theta)
-              :z (first (i/identity-matrix m))}
+        init {:at (m/new-matrix m 1)
+              :pt (m/identity-matrix m)
+              :t  (m/join-along 1
+                   (m/matrix phi)
+                   (m/join-along 0
+                    (m/identity-matrix (dec m))
+                    (m/new-matrix 1 (dec m))))
+              :h (m/add phi theta)
+              :z (first (m/identity-matrix m))}
         ll    (reduce log-likelihood* init ys)
-        sigma (-> (i/sq  (:vt ll))
-                  (i/div (:ft ll))
-                  (s/mean))
+        sigma (-> (m/square  (:vt ll))
+                  (m/div (:ft ll))
+                  (ms/mean))
         n (count ys)]
     (* -0.5 (+ n
-               (* n (i/log (* 2 Math/PI)))
-               (* n (i/log sigma))
-               (i/sum (i/log (:ft ll)))))))
+               (* n (m/log (* 2 Math/PI)))
+               (* n (m/log sigma))
+               (ms/sum (m/log (:ft ll)))))))
 
 (defn aic [coefs p q ys]
   (+ (* -2 (log-likelihood coefs p q ys))
